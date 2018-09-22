d6b2898f2456e4a9bced5ade8f43a14e8dc4e297
Harrison Grodin <harrison.grodin@gmail.com>
2018-09-22 14:26:34 -0400

    Implement endpoints and RethinkDB

--- app.py
+++ app.py
@@ -1,13 +1,74 @@
-from flask import Flask, jsonify
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+
+import rethinkdb as r
+from quel.models import Assignment, Availability, Course, from_iso, to_iso
+from quel.schedule import schedule
 
 app = Flask(__name__)
+conn = r.connect(db='quel')
 
 
-@app.route("/test")
-def test():
-    return jsonify({
-        "test": "success",
-    })
+@app.route("/availability/<uuid>", methods=['GET'])
+def availability(uuid):
+    return jsonify(r.table('availability').get(uuid).run(conn))
+
+
+@app.route("/availability", methods=['POST'])
+def post_availability():
+    uuid = r.table('availability') \
+            .insert(request.json) \
+            .run(conn)['generated_keys'][0]
+
+    return jsonify(r.table('availability').get(uuid).run(conn))
+
+
+@app.route("/course/<uuid>", methods=['GET'])
+def course(uuid):
+    return jsonify(r.table('course').get(uuid).run(conn))
+
+
+@app.route("/course", methods=['POST'])
+def post_course():
+    uuid = r.table('course') \
+            .insert(request.json) \
+            .run(conn)['generated_keys'][0]
+
+    return jsonify(r.table('course').get(uuid).run(conn))
+
+
+@app.route("/assignment/<uuid>", methods=['GET'])
+def assignment(uuid):
+    return jsonify(r.table('assignment').get(uuid).run(conn))
+
+
+@app.route("/assignment", methods=['POST'])
+def post_assignment():
+    uuid = r.table('assignment') \
+            .insert(request.json) \
+            .run(conn)['generated_keys'][0]
+
+    return jsonify(r.table('assignment').get(uuid).run(conn))
+
+
+@app.route("/schedule")
+def schedule_route():
+    assignments = list(map(
+        Assignment.from_json,
+        r.table('assignment').run(conn)
+    ))
+
+    available_times = {from_iso(a['start']): a['duration']
+                       for a in r.table('availability').run(conn)}
+
+    plan, remaining = schedule(assignments, available_times)
+    result = {a.id: _clean_plan(p) for (a, p) in plan.items()}
+    return jsonify(result)
+
+
+def _clean_plan(plan):
+    return {d.isoformat(): hours for (d, hours) in plan.items()}
 
 
 if __name__ == "__main__":
